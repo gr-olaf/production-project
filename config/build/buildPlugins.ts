97f0b1575c94d2dcb2da2ebc9292f638fc379504
Alexandr Zaycev <hunter290592@yandex.ru>
2023-06-13 14:18:32 +0300

    Fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -9,7 +9,7 @@ export function buildPlugins({
 	paths,
 	isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-	return [
+	const plugins = [
 		new HtmlWebpackPlugin({
 			template: paths.html,
 		}),
@@ -21,9 +21,12 @@ export function buildPlugins({
 		new webpack.DefinePlugin({
 			__IS_DEV__: JSON.stringify(isDev),
 		}),
-		new ReactRefreshWebpackPlugin({
-			overlay: false,
-		}),
-		new BundleAnalyzerPlugin({ openAnalyzer: false }),
 	];
+
+	if (isDev) {
+		plugins.push(new ReactRefreshWebpackPlugin({ overlay: false }));
+		plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+	}
+
+	return plugins;
 }

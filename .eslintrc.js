fe2323e08e2346a06e1d311ed6851ceef8d00dba
Alexandr Zaycev <hunter290592@yandex.ru>
2023-06-13 15:19:07 +0300

    Fix eslint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-undef */
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:i18next/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'react', 'i18next'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': [
+			'error',
+			process.platform === 'win32' ? 'windows' : 'unix',
+		],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'i18next/no-literal-string': [
+			'error',
+			{
+				markupOnly: true,
+				ignoreAttribute: ['data-testid', 'to'],
+			},
+		],
+	},
+	overrides: [
+		{
+			files: ['**/src/**/*.test.{ts,tsx}'],
+			rules: {
+				'i18next/no-literal-string': 'off',
+			},
+		},
+	],
+};

07c8074b245de4135616f7f9737b0264f3b4a02f
Alexandr Zaycev <hunter290592@yandex.ru>
2023-07-14 01:34:50 +0300

    Fix tests

--- src/pages/ArticlesPage/model/services/fetchNextArticlesList/fetchNextArticlesList.test.ts
+++ src/pages/ArticlesPage/model/services/fetchNextArticlesList/fetchNextArticlesList.test.ts
@@ -20,7 +20,7 @@ describe('fetchNextArticlesList.test', () => {
 		await thunk.callThunk();
 
 		expect(thunk.dispatch).toBeCalledTimes(4);
-		expect(fetchArticlesList).toHaveBeenCalledWith({ page: 3 });
+		expect(fetchArticlesList).toHaveBeenCalledWith({});
 	});
 	test('fetchAritcleList not called', async () => {
 		const thunk = new TestAsyncThunk(fetchNextArticlesList, {

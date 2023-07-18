c761a7415345754e4c37b2ccf7600d6063f1d7bb
Alexandr Zaycev <hunter290592@yandex.ru>
2023-07-18 12:11:33 +0300

    Add generate slice sript

---
+++ scripts/createSlice/templates/schemaTypeTemplate.js
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const firstCharUpperCase = require('../firstCharUpperCase');
+
+module.exports = (sliceName) => `export interface ${firstCharUpperCase(
+	sliceName
+)}Schema {
+    
+}`;

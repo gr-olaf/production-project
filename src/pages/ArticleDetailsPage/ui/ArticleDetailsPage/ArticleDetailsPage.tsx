52fbb39212e5efe5cc2ae86be89778212cde432f
Alexandr Zaycev <hunter290592@yandex.ru>
2023-08-24 16:42:44 +0300

    Add toggle flags and auto remove script

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,20 +1,21 @@
 import { ArticleDetails } from '@/entities/Article';
+import { ArticleRating } from '@/features/ArticleRating';
 import { ArticleRecommendationsList } from '@/features/ArticleRecommendationsList';
-import { FC, memo } from 'react';
-import { useParams } from 'react-router-dom';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import {
    DynamicModuleLoader,
    ReducersList,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { toggleFeatures } from '@/shared/lib/features';
+import { Card } from '@/shared/ui/Card';
 import { VStack } from '@/shared/ui/Stack';
 import { Page } from '@/widgets/Page';
+import { FC, memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
 import { articleDetailsPageReducer } from '../../model/slice';
 import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
 import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
-import { ArticleRating } from '@/features/ArticleRating';
-import { getFeatureFlag } from '@/shared/lib/features';
-import { Counter } from '@/entities/Counter';
 
 interface ArticleDetailsPageProps {
    className?: string;
@@ -26,15 +27,18 @@ const initialReducers: ReducersList = {
 
 const ArticleDetailsPage: FC<ArticleDetailsPageProps> = (props) => {
    const { className } = props;
+   const { t } = useTranslation('article-details');
    const { id } = useParams<{ id: string }>();
-   const isArticleRatingEnabled = getFeatureFlag('isArticleRatingEnabled');
-   const isCounterEnabled = getFeatureFlag('isCounterEnabled');
-
-   console.log(isArticleRatingEnabled);
 
    if (!id) {
       return null;
    }
+
+   const articleRatingCard = toggleFeatures({
+      name: 'isArticleRatingEnabled',
+      on: () => <ArticleRating articleId={id} />,
+      off: () => <Card>{t('Оценка статей скоро появится')}</Card>,
+   });
 
    return (
       <DynamicModuleLoader reducers={initialReducers} removeAfrerUnmount>
@@ -42,8 +46,7 @@ const ArticleDetailsPage: FC<ArticleDetailsPageProps> = (props) => {
             <VStack gap="16" max>
                <ArticleDetailsPageHeader />
                <ArticleDetails id={id} />
-               {isArticleRatingEnabled && <ArticleRating articleId={id} />}
-               {isCounterEnabled && <Counter />}
+               {articleRatingCard}
                <ArticleRecommendationsList />
                <ArticleDetailsComments id={id} />
             </VStack>

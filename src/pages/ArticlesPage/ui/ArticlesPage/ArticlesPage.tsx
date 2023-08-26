77e69031ed27c5a110740bd258b5ac026da4256c
Alexandr Zaycev <hunter290592@yandex.ru>
2023-08-26 16:46:52 +0300

    Add greeting modal for articles page

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -14,6 +14,7 @@ import { articlesPageReducer } from '../../model/slice/articlesPageSlice';
 import { ArticleInfiniteList } from '../ArticleInfiniteList/ArticleInfiniteList';
 import { ArticlesPageFilters } from '../ArticlesPageFilters/ArticlesPageFilters';
 import cls from './ArticlesPage.module.scss';
+import { ArticlePageGreeting } from '@/features/ArticlePageGreeting';
 
 interface ArticlesPageProps {
    className?: string;
@@ -48,6 +49,7 @@ const ArticlesPage: FC<ArticlesPageProps> = (props) => {
          >
             <ArticlesPageFilters />
             <ArticleInfiniteList className={cls.list} />
+            <ArticlePageGreeting />
          </Page>
       </DynamicModuleLoader>
    );
